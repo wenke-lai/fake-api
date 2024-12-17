90382608500e853343bf1d42f493ffc5af111254
wenke.lai <wenke.studio@gmail.com>
2024-12-17 22:00:29 +0800

    feat: add vite origins to CORS

--- api/main.py
+++ api/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",  # vite dev
+    "http://localhost:5174",  # vite preview
+]
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_credentials=True)
 
 
 @app.get("/ping")
